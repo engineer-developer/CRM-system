78b668c46dd8c37968840e22b9ea2c83581309d6
Alex Suslov <suslov.lexx@gmail.com>
2025-08-26 18:53:27 +0500

    upd urls.py: add path accounts

--- crm_system/crm_core/urls.py
+++ crm_system/crm_core/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("crm_app.urls", namespace="crm_app")),
+    path("accounts/", include("auth_app.urls")),
+    path("", include("crm_app.urls")),
 ]
