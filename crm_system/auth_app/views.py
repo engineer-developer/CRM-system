04be7333a5380727e5e67ec96eba6e699dda16ec
Alex Suslov <suslov.lexx@gmail.com>
2025-08-26 19:01:39 +0500

    upd auth_app/views.py: add CustomLoginView

--- crm_system/auth_app/views.py
+++ crm_system/auth_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+class CustomLoginView(LoginView):
+    """Представление login"""
+
+    template_name = "auth_app/login.html"
+    redirect_authenticated_user = True
+
 
 # Create your views here.
