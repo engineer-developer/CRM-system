6b352ede6f2b5162230909c8e242e8289a97e63a
Alex Suslov <suslov.lexx@gmail.com>
2025-08-27 21:15:44 +0500

    add users_app/views.py

--- crm_system/users_app/views.py
+++ crm_system/users_app/views.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls.base import reverse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.http.response import HttpResponseRedirect
 
-# Create your views here.
+from users_app.forms import UserCreateForm, UserUpdateForm
+
+
+class UsersListView(ListView):
+    """Представление списка пользователей"""
+
+    model = User
+    template_name = "users_app/users-list.html"
+    queryset = User.objects.filter(is_superuser=False).filter(is_active=True)
+    context_object_name = "users"
+
+
+class UserDetailView(DetailView):
+    """Представление информации о пользователе"""
+
+    model = User
+    template_name = "users_app/users-detail.html"
+
+
+class UserCreateView(CreateView):
+    """Представление для создания пользователя"""
+
+    model = User
+    template_name = "users_app/users-create.html"
+    form_class = UserCreateForm
+
+
+class UserUpdateView(UpdateView):
+    """Представление для редактирования пользователя"""
+
+    model = User
+    template_name = "users_app/users-edit.html"
+    form_class = UserUpdateForm
+
+    def get_success_url(self):
+        return reverse(
+            "users_app:user_details",
+            kwargs={"pk": self.object.pk},
+        )
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        if len(form.fields["groups"]) == 1:
+
+            return HttpResponseRedirect(self.get_success_url())
+
+        return response
