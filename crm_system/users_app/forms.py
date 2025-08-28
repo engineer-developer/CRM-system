1e15cfc2c6b3ebc8f6a74518fbfa71ded24e7a92
Alex Suslov <suslov.lexx@gmail.com>
2025-08-28 20:51:25 +0500

    upd users_app.forms.UserUpdateForm

--- crm_system/users_app/forms.py
+++ crm_system/users_app/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.forms import ModelForm
 from django import forms
 
@@ -36,6 +36,15 @@ class UserCreateForm(forms.ModelForm):
 
 
 class UserUpdateForm(ModelForm):
+    groups = forms.ModelChoiceField(
+        queryset=Group.objects.all(),
+        required=True,
+        initial="администратор",
+        widget=forms.Select(attrs={"class": "form-select"}),
+        label="Изменить роль пользователя",
+        help_text="",
+    )
+
     class Meta:
         model = User
         fields = ("last_name", "first_name", "email", "groups")
@@ -43,11 +52,21 @@ class UserUpdateForm(ModelForm):
             "last_name": forms.TextInput(attrs={"class": "form-control"}),
             "first_name": forms.TextInput(attrs={"class": "form-control"}),
             "email": forms.EmailInput(attrs={"class": "form-control"}),
-            "groups": forms.RadioSelect(),
         }
-        help_texts = {
-            "groups": "",
-        }
-        labels = {
-            "groups": "Изменить роль пользователя",
-        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        group = self.instance.groups.all()[0]
+        print(f"group: {group.name}")
+        self.fields["groups"].initial = group.pk
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            # Удаляем все группы и добавляем выбранную
+            user.groups.clear()
+            groups = self.cleaned_data["groups"]
+            print(f"cleaned_data: {groups}")
+            user.groups.add(self.cleaned_data["groups"])
+        return user
