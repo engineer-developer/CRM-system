9d4b0b38d1e782f15cb6461539529a5529b5e073
Alex Suslov <suslov.lexx@gmail.com>
2025-08-30 22:37:54 +0500

    upd products_app.models.Product

--- crm_system/products_app/models.py
+++ crm_system/products_app/models.py
@@ -4,6 +4,28 @@ from django.db import models
 class Product(models.Model):
     """Модель услуги"""
 
-    name = models.CharField(max_length=200, null=False, blank=False)
-    description = models.TextField(null=True, blank=True)
-    cost = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        null=False,
+        blank=False,
+        verbose_name="Наименование услуги",
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание",
+    )
+    cost = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        verbose_name="Стоимость",
+    )
+
+    def __str__(self):
+        return f"Услуга '{self.name}'"
