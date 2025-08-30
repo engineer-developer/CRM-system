7e925bbc0002053ae59de30079b835a6d555ee71
Alex Suslov <suslov.lexx@gmail.com>
2025-08-30 23:55:46 +0500

    del dir products_app

--- crm_system/products_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductsAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "products_app"
-    verbose_name = "Услуги"
