89ffc8dac955d0124140c8fb62ed61f99ca1f5bd
Alex Suslov <suslov.lexx@gmail.com>
2025-08-30 17:50:26 +0500

    add users_app.views.UserPasswordUpdateView

--- crm_system/users_app/views.py
+++ crm_system/users_app/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.db.models import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.urls.base import reverse
 from django.views.generic import (
+    View,
     ListView,
     DetailView,
     CreateView,
@@ -9,7 +14,7 @@ from django.views.generic import (
     DeleteView,
 )
 
-from users_app.forms import UserCreateForm, UserUpdateForm
+from users_app.forms import UserCreateForm, UserUpdateForm, UserPasswordForm
 
 
 class UsersListView(ListView):
@@ -57,4 +62,38 @@ class UserUpdateView(UpdateView):
         return reverse(
             "users_app:user_details",
             kwargs={"pk": self.object.pk},
+        )
+
+class UserPasswordUpdateView(View):
+    """Представление для изменения пароля пользователя"""
+
+    template_name = "users_app/users-password-update.html"
+
+    def get(self, request, *args, pk=None, **kwargs):
+        """Рендеринг формы изменения пароля пользователя"""
+        user = get_object_or_404(User, pk=pk)
+        form = UserPasswordForm()
+        context = {
+            "object": user,
+            "form": form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, pk=None, **kwargs):
+        """Обработка данных переданных в форму изменения пароля пользователя"""
+        user = User.objects.get(pk=pk)
+        form = UserPasswordForm(request.POST)
+
+        if not form.is_valid():
+            context = {"object": user, "form": form}
+            return render(request, self.template_name, context)
+
+        password = form.cleaned_data["password1"]
+        user.set_password(password)
+        user.save()
+        return HttpResponseRedirect(
+            reverse(
+                "users_app:user_details",
+                kwargs={"pk": user.pk},
+            )
         )
