5c0600fce01c2f1be4dcd12dae0def730719a7c1
Alex Suslov <suslov.lexx@gmail.com>
2025-08-30 18:15:05 +0500

    add model products_app.models.Product

--- crm_system/products_app/models.py
+++ crm_system/products_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """Модель услуги"""
+
+    name = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, null=False)
