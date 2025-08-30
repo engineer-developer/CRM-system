0ac48327edd1cd877292d523e54cc17f9533d4c7
Alex Suslov <suslov.lexx@gmail.com>
2025-08-30 17:51:40 +0500

    upd users_app/urls.py

--- crm_system/users_app/urls.py
+++ crm_system/users_app/urls.py
@@ -5,6 +5,8 @@ from users_app.views import (
     UserDetailView,
     UserCreateView,
     UserUpdateView,
+    UserPasswordUpdateView,
+    UserDeleteView,
 )
 
 
@@ -12,7 +14,13 @@ app_name = "users_app"
 
 urlpatterns = [
     path("users/new/", UserCreateView.as_view(), name="user_create"),
+    path("users/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
     path("users/<int:pk>/edit/", UserUpdateView.as_view(), name="user_edit"),
+    path(
+        "users/<int:pk>/pass/change/",
+        UserPasswordUpdateView.as_view(),
+        name="user_change_pass",
+    ),
     path("users/<int:pk>/", UserDetailView.as_view(), name="user_details"),
     path("users/", UsersListView.as_view(), name="users_list"),
 ]
