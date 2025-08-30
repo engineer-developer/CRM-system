8edd14b21ff512c4465023bdaf2695b6e42bcda5
Alex Suslov <suslov.lexx@gmail.com>
2025-08-31 04:06:51 +0500

    upd products app

--- crm_system/products/admin.py
+++ crm_system/products/admin.py
@@ -5,7 +5,10 @@ from products.models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """Админка для модели клиента"""
 
+    fields = ("name", "description", "cost")
     list_display = ("id", "name", "cost")
     list_display_links = ("name",)
+    search_fields = ("name", "description")
     ordering = ("name",)
