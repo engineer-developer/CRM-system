00fa3edc2323235f14c6d48605e61eb0a4a9e0dc
Alex Suslov <suslov.lexx@gmail.com>
2025-08-30 18:02:45 +0500

    init products_app

---
+++ crm_system/products_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+
+app_name = "products_app"
+
+urlpatterns = [
+    # path("users/new/", UserCreateView.as_view(), name="user_create"),
+    # path("users/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
+    # path("users/<int:pk>/edit/", UserUpdateView.as_view(), name="user_edit"),
+    # path(
+    #     "users/<int:pk>/pass/change/",
+    #     UserPasswordUpdateView.as_view(),
+    #     name="user_change_pass",
+    # ),
+    # path("users/<int:pk>/", UserDetailView.as_view(), name="user_details"),
+    # path("users/", UsersListView.as_view(), name="users_list"),
+]
