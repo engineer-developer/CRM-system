561ba2851447e20587ef4be8138a77806fd5ad5a
Alex Suslov <suslov.lexx@gmail.com>
2025-08-30 22:36:55 +0500

    add products_app.admin.ProductAdmin

--- crm_system/products_app/admin.py
+++ crm_system/products_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from products_app.models import Product
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "name", "cost")
+    list_display_links = ("name",)
+    ordering = ("name",)
