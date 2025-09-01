08f65ecfe842160b4492fdd689b28bde5786fb43
Alex Suslov <suslov.lexx@gmail.com>
2025-09-01 21:01:49 +0500

    add action contracts.admin.delete_contract_with_non_client

--- crm_system/contracts/admin.py
+++ crm_system/contracts/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from contracts.models import Contract
 
 
+@admin.action(description="Удаление контрактов без активных клиентов")
+def delete_contract_with_non_client(modeladmin, request, queryset):
+    """Action для удаления контрактов не имеющих активных клиентов"""
+    qs = queryset.filter(customer__isnull=True)
+    qs.delete()
+
+
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
     """Админка для модели контракта"""
@@ -16,7 +23,9 @@ class ContractAdmin(admin.ModelAdmin):
         "pk",
         "name",
         "cost",
+        "customer",
         "created_at",
     )
     list_display_links = ("name",)
     search_fields = ("name",)
+    actions = [delete_contract_with_non_client]
