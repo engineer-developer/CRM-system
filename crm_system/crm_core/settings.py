1233d3d630925affc292bc5dc97c3ddbdb826292
Alex Suslov <suslov.lexx@gmail.com>
2025-09-12 01:17:50 +0500

    add FIXTURE_DIRS

--- crm_system/crm_core/settings.py
+++ crm_system/crm_core/settings.py
@@ -135,6 +135,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Fixtures settings
+FIXTURE_DIRS = [BASE_DIR / "fixtures"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
