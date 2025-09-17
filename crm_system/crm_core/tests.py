11a3705604e70b5099224cd4c87e039e61469812
Alex Suslov <suslov.lexx@gmail.com>
2025-09-17 23:20:40 +0500

    fix tests.py

--- crm_system/crm_core/tests.py
+++ crm_system/crm_core/tests.py
@@ -6,10 +6,16 @@ User = get_user_model()
 
 
 class TestClientMixin:
+    """
+    Миксин для тестов, добавляющий фикстуру с данными и
+    создающий тестового клиента
+    """
+
     fixtures = ("db_data_full.json",)
 
     @classmethod
     def setUpClass(self):
+        """Подготовка тестов"""
         super().setUpClass()
         user = User.objects.get(username="admin")
         self.test_client = Client()
